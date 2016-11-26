c94de7cdbecddbba1966e84dea07323cb0a0d147
Soyoun Kim <makeajourney@gmail.com>
2016-11-26 17:29:26 +0900

    로그인 관련 기능 수정

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     url(r'', include('blog.urls')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page' : '/'}),
     url(r'^admin/', include(admin.site.urls)),
 ]
