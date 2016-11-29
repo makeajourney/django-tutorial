3c0d2a6e84d1fce6359a49f536afcbad44bd92c3
Soyoun Kim <makeajourney@gmail.com>
2016-11-29 17:11:43 +0900

    add comment

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -61,3 +62,16 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
